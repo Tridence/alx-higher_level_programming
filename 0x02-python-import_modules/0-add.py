49905c1f82b5409b53e9da5b930e00463b722cb1
tridence <brianwanjalabarasa@gmail.com>
2022-12-06 10:52:09 +0300

    returned

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-a = 1
-b = 2
+if __name__ == "__main__":
+    from add_0 import add
 
-def add(a, b):
-  return a + b
-
-print('{} + {} = {}'.format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
